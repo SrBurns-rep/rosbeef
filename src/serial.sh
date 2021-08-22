21cad660b0325c82dcd626503918dc46b142a265
SrBurns <caiobl.25@gmail.com>
2021-08-22 14:58:41 -0300

    Arduino ROS hello world

---
+++ src/serial.sh
@@ -0,0 +1,4 @@
+gnome-terminal -e "bash -c 'source /opt/ros/noetic/setup.bash && roscore'"
+gnome-terminal -e "bash -c 'source /opt/ros/noetic/setup.bash && rosrun rosserial_python serial_node.py /dev/ttyACM0'"
+sleep 2s
+gnome-terminal -e "bash -c 'source /opt/ros/noetic/setup.bash && rostopic echo chatter'"
