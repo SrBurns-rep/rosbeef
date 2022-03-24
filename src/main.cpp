0fcb015c2cf9161968626e35a6fe17ef348ba77c
SrBurns <caiobl.25@gmail.com>
2022-03-24 10:44:13 -0300

    Fixed motorDecode state machine

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,10 @@
 
 // Start of defines
     // function pins
-#define pinCLK    4   // Clock pin   DIR_CLK
-#define pinLTCH   12  // Latch pin   DIR_LATCH
-#define pinDS     8   // Data pin    DIR_SER
-#define pinEnable 7   // Enable pin  DIR_EN
+#define PIN_CLK    4   // Clock pin   DIR_CLK
+#define PIN_LTCH   12  // Latch pin   DIR_LATCH
+#define PIN_DS     8   // Data pin    DIR_SER
+#define PIN_ENABLE 7   // Enable pin  DIR_EN
 
     // Shift register pins position
 #define M1A 2
@@ -47,47 +47,22 @@
 #define MBACKWARDS  2
 #define MKEEP       3
 
-    // Minimun interval for a running motor in milliseconds
+    // Minimun interval for running a motor in milliseconds
 #define MIN_INTERVAL    50
 // End of defines
 
 // Start of function prototypes
-/**
- * @fn void timeoutMotors()
- * @brief turn off motors after a set time interval hass passed
- * @param null
- * @return null
-*/
 void timeoutMotors();
-
-/**
- * @fn void sendMotorResponse()
- * @brief sends motor response to the response node
- * @param void
- * @return void
-*/
-void sendMotorResponse();
-
-/**
- * @fn motorShieldWrite(byte ctrlByte)
- * @param ctrlByte motor control byte that contains the shield's output state
- * @return void
-*/
-void motorShieldWrite(byte ctrlByte);
-
-/**
- * @fn void motorCallback(const std_msgs::UInt32& input)
- * @param input Receives the 32bit message from the Subscriber node
- * @return void
-*/
-void motorCallback(const std_msgs::UInt32& input);
+void sendResponse();
+void shieldWrite(byte ctrlByte);
+void callback(const std_msgs::UInt32& input);
+byte motorDecode(uint32_t data);
 // End of function prototypes
 
-// Task array
 task_t tasks[] = 
 {
     {0, MIN_INTERVAL, timeoutMotors},
-    {0, 200, sendMotorResponse}
+    {0, 200, sendResponse}
 };
 
 // control messaging
@@ -97,13 +72,15 @@ bool gotCtrlMessage = false;
 ros::NodeHandle nh;
 std_msgs::String msg;
 ros::Publisher pub("motor-response", &msg);
-ros::Subscriber<std_msgs::UInt32> sub("motor-input", motorCallback);
+ros::Subscriber<std_msgs::UInt32> sub("motor-input", callback);
 
 // Task system object
 rosbeef taskManager(sizeof(tasks)/sizeof(*tasks));
 
 u32 motorTimer[4] = {0};
+u16 motorTimeleft[4] = {0};
 u8 motorIntervals[4] = {0};
+u8 motorIntensities[4] = {0};
 
 const u8 channelA[] = {M1A, M2A, M3A, M4A};
 const u8 channelB[] = {M1B, M2B, M3B, M4B};
@@ -115,53 +92,78 @@ SIGNAL(TIMER0_COMPA_vect)
     taskManager.runTaskTimerCore();
 }
 
-// Functions
+// Common block
+void setup()
+{
+    pinMode(PIN_CLK, OUTPUT);
+    pinMode(PIN_LTCH, OUTPUT);
+    pinMode(PIN_DS, OUTPUT);
+    pinMode(PIN_ENABLE, OUTPUT);
 
+    pinMode(M1PWM, OUTPUT);
+    pinMode(M2PWM, OUTPUT);
+    pinMode(M3PWM, OUTPUT);
+    pinMode(M4PWM, OUTPUT);
+
+    digitalWrite(PIN_ENABLE, LOW);
+
+    analogWrite(M1PWM, 0);
+    analogWrite(M2PWM, 0);
+    analogWrite(M3PWM, 0);
+    analogWrite(M4PWM, 0);
+
+    taskManager.rosbeefInit();
+
+    nh.initNode();
+    nh.advertise(pub);  // register the subscriber
+    nh.subscribe(sub);  // register the publisher
+}
+
+void loop()
+{
+    taskManager.runTasks();
+}
+
+// Functions
 void timeoutMotors()
 {
-    static byte motorCtrl = 0;
     bool hasTimeout = false;
+    uint32_t code = 0xff000000;
     uint32_t current = millis();
     for(int i = 0; i < 4; i++){
 
-        if(motorTimer[i] > current) motorTimer[i] = 0; // overflow occurred
+        if(motorTimer[i] > current) motorTimer[i] = 0; // timer overflow occurred
 
-        if((u32)(current - motorTimer[i]) >= (u32)(MIN_INTERVAL * motorIntervals[i])){
-            bitClear(motorCtrl, channelA[i]);
-            bitClear(motorCtrl, channelB[i]);
+        if((u32)(current - motorTimer[i]) >= (u32)(MIN_INTERVAL * motorIntervals[i])) {
             motorTimer[i] = current;
+            motorTimeleft[i] = 0;
+            code &= (uint32_t)(~(0x03 << (30 - 2*i)));
             hasTimeout = true;
+        } else {
+            motorTimeleft[i] = (u16)(current - motorTimer[i]);
         }
     }
     if(hasTimeout){
-        motorShieldWrite(motorCtrl);
+        shieldWrite(motorDecode(code));
     }
 }
 
-void sendMotorResponse()
+void sendResponse()
 {
-    const char* b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                      "abcdefghijklmnopqrstuvwxyz"
-                      "0123456789+/";
-    const char pdn = '=';
-
     if(gotCtrlMessage){
+        snprintf(buffer, sizeof buffer, "%lu,%u,%u,%u,%u", millis(),
+        motorIntensities[0], motorIntensities[1], motorIntensities[2], motorIntensities[3]);
+        // ROS deve ter coisa pra isso
+        // Ver no firefox: ROS / rosserial
+        msg.data = buffer;
+        pub.publish(&msg);
+        nh.spinOnce();
+
         gotCtrlMessage = false;
-
-        u32 timestamp = millis();
-        int i = 0;
-
-        /*for(i; i < 32; i+=6){
-            buffer[i] = b64[(timestamp >> (32 - 6 - i))& 0x3f];
-        }
-        i++;
-        buffer[i] = pdn;
-        for(i; i < )*/
-        // TO-DO
     }
 }
 
-void motorShieldWrite(byte ctrlByte)
+void shieldWrite(byte ctrlByte)
 {
     // Disable interrupts
     cli();
@@ -190,16 +192,22 @@ void motorShieldWrite(byte ctrlByte)
     sei();
 }
 
-void motorCallback(const std_msgs::UInt32& input)
+void callback(const std_msgs::UInt32& input)
 {
+    byte ctrl = motorDecode(input.data);
+    shieldWrite(ctrl);
+    gotCtrlMessage = true;
+}
+
+byte motorDecode(uint32_t data){
     const u8 pwms[] = {M1PWM, M2PWM, M3PWM, M4PWM};
-    static byte motorCtrl = 0;
-    u32 data = input.data;
+    static byte motorCtrl = 0;  //maintain data
 
     //m1 m2 m3 m4 t1 t2 t3 t4 i1 i2 i3 i4
     // 2  2  2  2  2  2  2  2  4  4  4  4
-    // stop   forward   backward    maintain
+    // stop   forward   backward    keep
     // 00     01        10          11
+
     for(int i = 0; i < 4; i++)
     {
         u8 c = (data >> (30 - 2*i)) & 0x03;
@@ -210,59 +218,32 @@ void motorCallback(const std_msgs::UInt32& input)
                           + (interval == 1)*5
                           + (interval == 2)*50
                           + (interval == 3)*200;
-        
-        digitalWrite((int)pwms[i], gain);
+        motorIntensities[i] = gain;
+
+        if(c != MKEEP){
+            digitalWrite(pwms[i], gain);
+        }
 
         switch(c)
         {
             case(MSTOP):;
                 bitClear(motorCtrl, channelA[i]);
                 bitClear(motorCtrl, channelB[i]);
+                digitalWrite(pwms[i], 0);  // stop motor pwm
                 break;
             case(MFORWARDS):;
                 bitSet(motorCtrl, channelA[i]);
                 bitClear(motorCtrl, channelB[i]);
+                motorTimer[i] = millis();
                 break;
             case(MBACKWARDS):;
                 bitClear(motorCtrl, channelA[i]);
                 bitSet(motorCtrl, channelB[i]);
+                motorTimer[i] = millis();
                 break;
             case(MKEEP):;
                 break;
         }
     }
-    motorShieldWrite(motorCtrl);
-    gotCtrlMessage = true;
-}
-
-// Arduino block
-void setup()
-{
-    pinMode(pinCLK, OUTPUT);
-    pinMode(pinLTCH, OUTPUT);
-    pinMode(pinDS, OUTPUT);
-    pinMode(pinEnable, OUTPUT);
-
-    pinMode(M1PWM, OUTPUT);
-    pinMode(M2PWM, OUTPUT);
-    pinMode(M3PWM, OUTPUT);
-    pinMode(M4PWM, OUTPUT);
-
-    digitalWrite(pinEnable, LOW);
-
-    analogWrite(M1PWM, 0);
-    analogWrite(M2PWM, 0);
-    analogWrite(M3PWM, 0);
-    analogWrite(M4PWM, 0);
-
-    taskManager.rosbeefInit();
-
-    nh.initNode();
-    nh.advertise(pub);  // register the subscriber
-    nh.subscribe(sub);  // register the publisher
-}
-
-void loop()
-{
-    taskManager.runTasks();
+    return motorCtrl;
 }
